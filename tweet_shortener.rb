3a0c69eee7949d09c1090f00cd332ffb5ff9e05f
Jon Schulman <schulman99@gmail.com>
2018-07-04 13:42:48 +0000

    automatically backed up by learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -16,6 +16,39 @@ dictionary = {
 end
 
 def word_substituter (tweet)
-  arr_tweet = tweet.split(" ")
-binding.pry
+  trigger_words = dictionary.keys
+  tweet_array = tweet.split(" ")
+  new_tweet = []
+  tweet_array.each do |word|
+    if dictionary.include?(word)
+      new_tweet.push(dictionary[word])
+    else new_tweet.push(word)
+    end
+  end
+  new_tweet.join(" ")
+end
+
+def bulk_tweet_shortener (twitters)
+  trigger_words = dictionary.keys
+  new_tweets_together = []
+  twitters.each do |full_tweet|
+    new_tweet = []
+    tweet_array = full_tweet.split(" ")
+    tweet_array.each do |word|
+      if dictionary.include?(word)
+        new_tweet.push(dictionary[word])
+      else new_tweet.push(word)
+      end
+# binding.pry
+    new_tweet.join(" ")
+    end
+    new_tweets_together.push(new_tweet)
+    # puts new_tweets_together.join(" ")
+# binding.pry
+    new_tweets_together.join(" ")
+
+  end
+    new_tweets_together.each do |puts_me|
+      puts puts_me
+    end
 end
