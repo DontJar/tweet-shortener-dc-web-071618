797e788e82542b11bdfc64366a649b2f61d858e0
Jon Schulman <schulman99@gmail.com>
2018-07-04 14:56:11 +0000

    automatically backed up by learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -47,5 +47,24 @@ def bulk_tweet_shortener (lots_of_tweets)
   end
 end
 
-def selective_tweet_shortener
+def selective_tweet_shortener (lots_of_tweets)
+  new_tweets_together = []
+  new_full_tweet = []
+
+  lots_of_tweets.each do |each_tweet|
+    new_tweet = []
+    tweet_words = each_tweet.split(" ")
+
+    if tweet.words.size <= 140
+      puts each_tweet      
+    else tweet_words.each do |word|
+        if dictionary.include?(word)
+          new_tweet.push(dictionary[word])
+        else new_tweet.push(word)
+        end
+      new_full_tweet = new_tweet.join(" ")
+      end
+      puts new_full_tweet
+    end
+  end
 end
